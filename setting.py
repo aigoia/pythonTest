9be4c9f99e4db4beef436d0ebe3cc9fe1ed8d5c0
aigoia <oidoicompany@gmail.com>
2024-12-24 04:08:26 +0900

    cleaning

--- setting.py
+++ setting.py
@@ -1,21 +1,26 @@
 from pyray import *
 
+# Game settings
 game_name = "Pong"
 frame = 60
 screen_width = 1280
 screen_height = 800
 
+# Colors
 Green = Color(38, 185, 154, 255)
 Yellow = Color(243, 213, 91, 255)
 
+# Margins
 paddle_magin = 10
 score_magin = 20
-scoreSize = 80
 
+# Sizes
+scoreSize = 80
 paddle_width = 25
 paddle_height = 120
 ball_size = 12
 
+# Speeds
 player_speed = 12
 cpu_speed = 6
 ball_speed = 7
